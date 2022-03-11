9135a8b24f15bb8d4f24678419719cd08e9c8b8b
Royer Jack <jack.royer.23@gmail.com>
2022-03-11 12:43:25 +0100

    Solved euler 51

--- 1-100/51-60/euler51.hs
+++ 1-100/51-60/euler51.hs
@@ -1,19 +1,23 @@
--- The number of digits
-n :: Int
-n = 6
+import Data.List
+import Utils.Miscellaneous (digits2num, num2digits)
+import Utils.Primes (isPrime, primes)
 
-digits :: [Int]
-digits = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+-- | `replace x y l` replaces `x` by `y` in the list `l`
+replace :: Eq a => a -> a -> [a] -> [a]
+replace x y [] = []
+replace x y (h : t)
+  | h == x = y : replace x y t
+  | otherwise = h : replace x y t
 
-numList :: Int -> [Int]
-numList n
-    | q == 0    = [r]
-    | otherwise = r : numList q
-    where (q, r) = quotRem n 10
+isPart8Prime :: [Integer] -> Bool
+isPart8Prime l = foo 0
+  where
+    foo d
+      | d >= 10 = False
+      | numberD < 2 = foo (d + 1)
+      | otherwise = ((< 3) . length . filter (not . (\x -> x > 10 ^ 4 && isPrime x) . digits2num) $ [replace d n l | n <- [0 .. 9], n /= d]) || foo (d + 1)
+      where
+        numberD = length . filter (== d) $ l
 
-matches :: (Eq a, Num a) => [a] -> [a] -> Bool
-matches (x:xs) []       = (x == 0 || x == -1) && matches xs []
-matches (-1:xs) (_:ys)  = matches xs ys
-matches (x:xs) (y:ys)   = x == y && matches xs ys
-matches _ _ = True
-
+main :: IO ()
+main = print . head . filter (isPart8Prime . num2digits) . takeWhile (< 10 ^ 6) . dropWhile (< 10 ^ 4) $ primes
