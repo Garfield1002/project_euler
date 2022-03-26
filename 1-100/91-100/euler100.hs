54e9bcf578780a79df5377855d974b263ea7a619
Royer Jack <jack.royer.23@gmail.com>
2022-03-26 15:11:28 +0100

    Solved euler 100

--- 1-100/91-100/euler100.hs
+++ 1-100/91-100/euler100.hs
@@ -1,26 +1,8 @@
-import Data.Number.CReal
-
-dico :: Integer -> Integer
-dico n = dico' (div n 2) n (div n 2)
-    where dico' l u b
-            | 2 * (b^2 - b) == n^2- n           = b
-            | u - l <= 1                            = -1
-            | div (2 * (b^2 - b)) (n^2- n) == 0     = dico' b u (div (b+u) 2)
-            | otherwise                             = dico' l b (div (l+b) 2)
-
-
-f :: Integral a2 => a2 -> CReal
-f b = (1 + sqrt (fromIntegral (1 + 8*(b*(b-1))))) / 2
-
-isInt :: CReal -> Bool
-isInt x = x == fromInteger (round x)
-
-demi = 2 * 707106788769 * (707106788769 - 1) == 1000000010723 * (1000000010723 - 1)
-
-euler100 = [(b, n) |
-   b <- [707106780000..],
-   let n = f b,
-   let t = floor n,
-   fromIntegral (2*b*(b-1)) == t*(t-1)]
-
-main = print $ head euler100
+-- the coefficients used to solve the generic two integer variable equation where found here
+-- https://www.alpertron.com.ar/QUAD.HTM
+euler100 :: Int
+euler100 = next 15 21
+  where
+    next x y
+      | y < 10 ^ 12 = next (3 * x + 2 * y - 2) (4 * x + 3 * y - 3)
+      | otherwise = x
